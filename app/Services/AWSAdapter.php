cdc20ed14a47d51177168789c81e717eb6480fe5
kckct <ss010359@gmail.com>
2017-05-18 23:06:02 +0800

    加入AWSAdapter.php

---
+++ app/Services/AWSAdapter.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services;
+
+use App\SDK\AWSSDK;
+
+/**
+ * Class AWSAdapter
+ * @package App\Services
+ */
+class AWSAdapter implements CloudSDK
+{
+    /*** @var AWSSDK */
+    private $awsSDK;
+
+    /**
+     * AWSAdapter constructor.
+     * @param AWSSDK $awsSDK
+     */
+    public function __construct(AWSSDK $awsSDK)
+    {
+        $this->awsSDK = $awsSDK;
+    }
+
+    /**
+     * @param string $container
+     * @param string $blob
+     * @return string
+     */
+    public function getObject(string $container, string $blob): string
+    {
+        return $this->awsSDK->getObject($container, $blob);
+    }
+
+    /**
+     * @param string $container
+     * @param string $blob
+     * @param string $file
+     * @return string
+     */
+    public function putObject(string $container, string $blob, string $file): string
+    {
+        return $this->awsSDK->putObject($container, $blob, $file);
+    }
+
+    /**
+     * @param string $container
+     * @param string $blob
+     * @return string
+     */
+    public function deleteObject(string $container, string $blob): string
+    {
+        return $this->awsSDK->deleteObject($container, $blob);
+    }
+}
